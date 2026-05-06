f44a2a5d8369846e62902627d141cd34274829d3
Rahul Singh <rahulsingh51775177@gmail.com>
2026-05-06 20:09:15 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -1,4 +1,22 @@
 ## Python
+class Solution {
+  public:
+    int ans;
+    void solve(Node* root){
+        if(!root){
+            return;
+        }
+        solve(root->left);
+        ans++;
+        solve(root->right);
+    }
+    
+    int getSize(Node* root) {
+        ans = 0;
+        solve(root);
+        return ans;
+    }
+};
 
 class Solution:
     def rotateRight(self, head, k):
