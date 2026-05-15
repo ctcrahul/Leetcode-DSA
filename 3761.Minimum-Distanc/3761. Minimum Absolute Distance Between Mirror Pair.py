01b843a8279c0f2a98b371dd95998826b0043a6b
Rahul Singh <rahulsingh51775177@gmail.com>
2026-05-15 20:26:51 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -1,6 +1,12 @@
 ## Python
 
 class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        return *partition_point(nums.begin(), nums.end(), [&](int n) { return n > nums.back(); });
+    }
+};
+class Solution {
   public:
     int optimalKeys(int n) {
         
