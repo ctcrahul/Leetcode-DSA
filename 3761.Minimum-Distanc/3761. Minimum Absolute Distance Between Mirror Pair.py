ba45b89c99b36699fe2f145f36f9a0d89275e8b7
Rahul Singh <rahulsingh51775177@gmail.com>
2026-05-20 20:27:18 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -1,4 +1,23 @@
+
+
 ## Python
+
+
+const findThePrefixCommonArray = (A, B) => {
+    const seen = [];
+    let count = 0;
+
+    return A.map((c, i) => {
+        if (seen[c]) count++;
+        else seen[c] = true;
+
+        if (seen[B[i]]) count++;
+        else seen[B[i]] = true;
+
+        return count;
+    });
+};
+
 
 
 class Solution {
