d834043ad9c2a3ea53785998c82bb23047e06b92
Rahul Singh <rahulsingh51775177@gmail.com>
2026-05-20 20:27:04 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -1,5 +1,28 @@
 ## Python
 
+
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size(), count = 0;
+        vector<int> res(n);
+        bitset<51> seen;
+
+        for (int i = 0; i < n; i++) {
+            count += seen[A[i]];
+            seen.set(A[i]);
+
+            count += seen[B[i]];
+            seen.set(B[i]);
+
+            res[i] = count;
+        }
+
+        return res;
+    }
+};
+
+
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         n = len(A)
