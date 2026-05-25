f519568773b21eda4b03f43228a6dded0f80ae59
Rahul Singh <rahulsingh51775177@gmail.com>
2026-05-25 21:10:37 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -20,6 +20,31 @@ class Solution:
                 dp[i] = True
                 maxR = i + maxJ
 
+        return reach > 0https://github.com/ctcrahul
+        
+
+class Solution:
+    def canReach(self, s: str, minJ: int, maxJ: int) -> bool:
+        n = len(s)
+
+        if int(s[-1]): return False
+
+        dp = [False] * n
+        dp[0] = True
+        reach, maxR = 0, maxJ
+
+        for i in range(minJ, n):
+            if i > maxR: return False
+
+            reach += dp[i - minJ]
+
+            if i > maxJ:
+                reach -= dp[i - maxJ - 1]
+
+            if reach and not int(s[i]):
+                dp[i] = True
+                maxR = i + maxJ
+
         return reach > 0
 
 class Solution {
