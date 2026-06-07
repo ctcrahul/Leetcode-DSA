a1702930c22cd769184eb94b0e3b58f1f4ea9937
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-07 21:02:50 +0530

    Add createBinaryTree method for binary tree construction
    Implemented a method to create a binary tree from a given vector of vectors, mapping nodes and establishing left/right relationships.

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -1,4 +1,34 @@
 class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& A) {
+        unordered_map<int, TreeNode*> nodes;
+        nodes.reserve(A.size() + 1);
+        int root = 0;
+
+        for (auto& d : A) {
+            int x = d[0], y = d[1], isLeft = d[2];
+            if (!nodes.contains(x)) {
+                nodes[x] = new TreeNode(x);
+                root ^= x;
+            }
+            if (!nodes.contains(y)) {
+                nodes[y] = new TreeNode(y);
+                root ^= y;
+            }
+
+            if (isLeft)
+                nodes[x]->left = nodes[y];
+            else
+                nodes[x]->right = nodes[y];
+            root ^= y;
+        }
+
+        return nodes[root];
+    }
+};
+
+
+class Solution {
     public TreeNode createBinaryTree(int[][] A) {
         Map<Integer, TreeNode> nodes = new HashMap<>(A.length + 1, 1);
         int root = 0;
