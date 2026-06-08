b10dd167a433691717318cd68b99cae8635e4d8a
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-08 20:57:38 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -1,3 +1,23 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        n=len(nums)
+        if n==1: return nums
+        R=[]
+        l, m=0, 0
+        for x in nums:
+            if x<pivot:
+                nums[l]=x
+                l+=1
+            elif x>pivot:
+                R.append(x)
+            else:
+                m+=1
+        return nums[:l]+[pivot]*m+R
+        
+
+
+
+
 class Solution {
 public:
     static vector<int> pivotArray(vector<int>& nums, int pivot) {
