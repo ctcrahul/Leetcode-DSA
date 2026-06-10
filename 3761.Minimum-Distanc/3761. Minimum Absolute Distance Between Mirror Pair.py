f3d0ec5d9bdaddf0af1dfb8fbc1d0ccc185bbfea
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-10 21:46:54 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -47,6 +47,46 @@ class Solution {
 };
 
 
+
+
+class Solution {
+  public:
+    bool canSeatAllPeople(int k, vector<int> &seats) {
+        // code here
+        int n=seats.size();
+        for(int i=1; i<n; i++){
+            if(seats[i]==1 && seats[i-1]==1) return 0;
+        }
+        if(k==1 && n==1){
+            return seats[0]==0?1:0;
+        } 
+
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            if(i==0 && seats[i]==0){
+                if(i+1<n && seats[i+1]==0){
+                    seats[i]=1;
+                    cnt++;
+                }
+            }else if(i==n-1 && seats[i]==0){
+                if(i-1>=0 && seats[i-1]==0){
+                    seats[i]=1;
+                    cnt++;
+                }
+            }else{
+                if(seats[i-1]==0 && seats[i+1]==0 && seats[i]==0){
+                    seats[i]=1;
+                    cnt++;
+                }
+            }
+        }
+
+        if(cnt>=k) return true;
+        return false;
+    }
+};
+
+
 class Solution:
     def maxTotalValue(self, A: List[int], k: int) -> int:
         gMin = gMax = A[0]
