29f2cde66a34ebef5251bf54fdd2242fffb3ffe0
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-13 19:23:49 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -7,6 +7,25 @@ Deep Learning
 Generative AI
 ⬇️
 LLMs
+
+
+
+
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        ans = []
+
+        for word in words:
+            total = 0
+
+            for ch in word:
+                total += weights[ord(ch) - ord('a')]
+
+            remainder = total % 26
+            ans.append(chr(ord('z') - remainder))
+
+        return "".join(ans)
+		
 func assignEdgeWeights(edges [][]int, queries [][]int) []int {
 	const MOD = 1000000007
 
