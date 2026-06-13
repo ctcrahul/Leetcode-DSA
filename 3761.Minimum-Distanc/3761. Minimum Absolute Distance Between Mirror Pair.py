3260144bef96860b4a3851ff523c5395dbd24d0a
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-13 19:24:15 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -12,6 +12,26 @@ LLMs
 
 
 class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+            long sum = 0;
+
+            for (char ch : word.toCharArray()) {
+                sum += weights[ch - 'a'];
+            }
+
+            int remainder = (int)(sum % 26);
+            ans.append((char)('z' - remainder));
+        }
+
+        return ans.toString();
+    }
+}
+
+
+class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         string ans;
