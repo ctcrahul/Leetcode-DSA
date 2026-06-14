7f371d078e9f82472b1c0d6811abd9cd4524d084
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-14 21:08:51 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,6 +9,30 @@ Generative AI
 LLMs
 
 
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            swap(slow->next, prev);
+            swap(prev, slow);
+        }
+
+        int res = 0;
+        while (slow) {
+            res = max(res, prev->val + slow->val);
+            prev = prev->next;
+            slow = slow->next;
+        }
+
+        return res;
+    }
+};
+
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         slow = fast = head
