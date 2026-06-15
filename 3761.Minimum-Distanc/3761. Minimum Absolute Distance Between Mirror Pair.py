9a5b65ba2c6e92582e069cf474ccbd795f358f7b
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-15 21:28:35 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,6 +9,22 @@ Generative AI
 LLMs
 
 
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next: return None
+
+        slow = head
+        fast = slow.next.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        slow.next = slow.next.next
+        return head
+		
+
+
 class Solution {
 public:
     int pairSum(ListNode* head) {
