ba2f39f772e5942be7647f246ba932f984929baf
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-16 20:54:44 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,6 +9,38 @@ Generative AI
 LLMs
 
 
+
+//translated using AI
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+
+            if (ch == '*') {
+                if (res.length() != 0) {
+                    res.pop_back();
+                }
+            } 
+            else if (ch == '#') {
+                res += res;
+            } 
+            else if (ch == '%') {
+                reverse(res.begin(), res.end());
+            } 
+            else if (ch >= 'a' && ch <= 'z') {
+                res.push_back(ch);
+            }
+        }
+
+        return res;
+    }
+};
+
+
 #translated using AI
 class Solution:
     def processStr(self, s: str) -> str:
