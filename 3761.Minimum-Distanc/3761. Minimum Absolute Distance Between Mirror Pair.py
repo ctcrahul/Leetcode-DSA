06b3a656031c9ea8c947d041811962e02e6e13ce
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-17 21:54:33 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,6 +9,43 @@ Generative AI
 LLMs
 
 
+const processStr = (s, k) => {
+    const n = s.length;
+    const lens = [];
+    let len = 0;
+
+    for (const c of s) {
+        if (c === '*')
+            len = Math.max(len - 1, 0);
+        else if (c === '#')
+            len *= 2;
+        else if (c !== '%')
+            len++;
+        
+        lens.push(len);
+    }
+
+    if (k >= len) return '.';
+
+    for (let i = n - 1; ; i--) {
+        switch (s[i]) {
+            case '*':
+                break;
+            case '#':
+                if (k >= Math.floor(lens[i] / 2))
+                    k -= Math.floor(lens[i] / 2);
+                break;
+            case '%':
+                k = lens[i] - 1 - k;
+                break;
+            default:
+                if (lens[i] === k + 1)
+                    return s[i];
+        }
+    }
+};
+
+
 class Solution {
     typedef long long ll;
 public:
