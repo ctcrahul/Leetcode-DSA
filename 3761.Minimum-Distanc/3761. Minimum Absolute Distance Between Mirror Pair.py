7c0d7620ee974cefbc605ac1cb42d88957ff0307
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-18 20:38:51 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,6 +9,12 @@ Generative AI
 LLMs
 
 
+const angleClock = (hour, minutes) => {
+    const x = hour + minutes / 60;
+    const diff = (11 * x) % 12;
+    return Math.min(diff, 12 - diff) * 30;
+};
+
 
 class Solution {
 public:
