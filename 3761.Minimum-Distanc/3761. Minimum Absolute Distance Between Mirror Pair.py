06f1f0311374a663c6130e6436e8de6744358ebd
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-18 20:38:37 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -10,6 +10,14 @@ LLMs
 
 
 
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double x = hour + minutes / 60.0;
+        double diff = fmod(11.0 * x, 12.0);
+        return min(diff, 12.0 - diff) * 30.0;
+    }
+};
 
 class Solution:
     def angleClock(self, hour: int, minutes: int) -> float:
