8fe90b91e5e5daced1f417d83949f87280944d14
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-19 21:28:43 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -11,6 +11,24 @@ LLMs
 
 
 class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans = 0, sum = 0;
+
+        for (auto& it : gain) {
+            sum += it;
+            int d = sum - ans;
+            ans += d & ~(d >> 0x1F);
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+class Solution {
   public:
     vector<int> optimalArray(vector<int> &arr) {
         int n = arr.size();
