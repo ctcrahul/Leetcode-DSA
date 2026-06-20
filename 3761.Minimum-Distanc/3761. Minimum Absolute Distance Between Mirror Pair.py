0b76396a87696c6893b72f616d1cab6e2254f55e
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-20 21:54:53 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,6 +9,29 @@ Generative AI
 LLMs
 
 
+const { abs, max, min } = Math;
+
+const maxBuilding = (num, r) => {
+    r.push([1, 0]);
+    r.sort(([a], [b]) => a - b);
+    const n = r.length;
+
+    for (let i = 1; i < n; i++)
+        r[i][1] = yCap(r[i - 1], r[i]);
+
+    for (let i = n - 2; i >= 0; i--)
+        r[i][1] = yCap(r[i + 1], r[i]);
+
+    let res = 0;
+    for (let i = 1; i < n; i++)
+        res = max(res, yPeak(r[i - 1], r[i]));
+
+    return max(res, r[n - 1][1] + num - r[n - 1][0]);
+};
+
+const yCap = ([x1, y1], [x2, y2]) => min(y2, y1 + abs(x2 - x1));
+const yPeak = ([x1, y1], [x2, y2]) => (y1 + y2 + x2 - x1) >> 1;
+
 class Solution {
 public:
     int maxBuilding(int num, vector<vector<int>>& r) {
