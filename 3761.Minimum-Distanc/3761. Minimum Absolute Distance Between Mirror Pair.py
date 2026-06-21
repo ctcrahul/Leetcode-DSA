dcf8a660c24d51263c4ea275a01018cba0da3faa
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-21 21:46:23 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,7 +9,44 @@ Generative AI
 LLMs
 
 
-#pragma GCC optimize("O3, unroll-loops")
+
+constexpr int M = 1e5 + 1;
+int freq[M];
+class Solution {
+public:
+    static int maxIceCream(vector<int>& costs, int coins) {
+        int xMax = 0;
+        for (int x : costs) {
+            freq[x]++;
+            xMax = max(xMax, x);
+        }
+        int cnt = 0;
+        for (int x = 1; x <= xMax; x++) {
+            const int f = freq[x];
+            if (f == 0)
+                continue;
+            int buy = min(coins / x, f);
+            if (buy == 0)
+                break;
+            cnt += buy;
+            coins -= buy * x;
+        }
+        // reset for the next testcase
+        for (int x : costs)
+            freq[x] = 0;
+        return cnt;
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+
+#pragma GCC optimize("O3, unroll
+-loops")
 static int freq[100001] = {0};
 static int maxIceCream(int* costs, int n, int coins) {
     int xMax = 0;
