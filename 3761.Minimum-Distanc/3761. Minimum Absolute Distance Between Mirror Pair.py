95a84390f9659b29a1260dca7f1a152155fef3ef
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-21 21:46:10 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -9,6 +9,32 @@ Generative AI
 LLMs
 
 
+#pragma GCC optimize("O3, unroll-loops")
+static int freq[100001] = {0};
+static int maxIceCream(int* costs, int n, int coins) {
+    int xMax = 0;
+    for (int i = 0; i < n; i++) {
+        const int x = costs[i];
+        freq[x]++;
+        xMax += (-(x > xMax) & x - xMax);
+    }
+    int cnt = 0;
+    for (int x = 1; x <= xMax && coins > 0; x++) {
+        const int f = freq[x];
+        if (f == 0)
+            continue;
+        int q = coins / x;
+        int buy = fmin(q, f);
+        cnt += buy;
+        coins -= buy * x;
+    }
+    // reset
+    for (int i = 0; i < n; i++)
+        freq[costs[i]] = 0;
+    return cnt;
+}
+
+
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
 
