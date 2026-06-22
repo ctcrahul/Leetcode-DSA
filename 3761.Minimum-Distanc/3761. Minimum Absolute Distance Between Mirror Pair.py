363b4400786c1a8d9178f13a2347d8577da8b0b7
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-22 20:14:38 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -10,6 +10,13 @@ LLMs
 
 
 
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        f = Counter(text)
+        return min(f["b"], f["a"], f["l"] >> 1, f["o"] >> 1, f["n"])
+		
+
+
 constexpr int M = 1e5 + 1;
 int freq[M];
 class Solution {
