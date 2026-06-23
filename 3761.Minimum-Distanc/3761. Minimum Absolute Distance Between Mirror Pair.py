ffc4fdb909599cb3d04d7daaa03a910e9363a2f3
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-23 21:44:40 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -10,6 +10,20 @@ LLMs
 
 
 class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        MOD = 1000000007
+        m = r - l + 1
+        dp = [1] * m
+
+        for i in range(2, n + 1):
+            dp.reverse()
+            s = 0
+            for j in range(m):
+                dp[j], s = s, (s + dp[j]) % MOD
+
+        return (sum(dp) % MOD << 1) % MOD
+
+class Solution:
     def maxArea(self, height):
         # code here
         i, j = 0, len(height) - 1
