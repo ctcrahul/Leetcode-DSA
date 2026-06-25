4d60dbfe5964263696a41bd4d364825a05d0ce2f
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-25 21:01:05 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,56 @@ LLMs
 ...
 
 
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        long long ans = 0;
+
+        for (int l = 0; l < n; l++) {
+            int targetCount = 0;
+
+            for (int r = l; r < n; r++) {
+                if (nums[r] == target) {
+                    targetCount++;
+                }
+
+                int len = r - l + 1;
+
+                if (targetCount > len / 2) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        long long ans = 0;
+
+        for (int l = 0; l < n; l++) {
+            int targetCount = 0;
+
+            for (int r = l; r < n; r++) {
+                if (nums[r] == target) {
+                    targetCount++;
+                }
+
+                int len = r - l + 1;
+
+                if (targetCount > len / 2) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
 var countMajoritySubarrays = function(nums, target) {
     const n = nums.length;
     let ans = 0;
