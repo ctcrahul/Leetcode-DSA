7c428245d63e3ef236e1b029a07ee13a5e7e1a13
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-26 21:51:08 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -12,7 +12,24 @@ LLMs
 
 
 ...
+class Solution:
+    def countMajoritySubarrays(self, nums, target):
+        n = len(nums)
+        cnt = 0
 
+        for i in range(n):
+            freq = 0
+
+            for j in range(i, n):
+                if nums[j] == target:
+                    freq += 1
+
+                length = j - i + 1
+
+                if freq > length // 2:
+                    cnt += 1
+
+        return cnt
 
 func countMajoritySubarrays(nums []int, target int) int64 {
     n := len(nums)
