09a2e9c45af3ba2906b4b464ccb207c3d2db27e3
Rahul Singh <rahulsingh51775177@gmail.com>
2026-06-30 20:46:26 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,24 @@ LLMs
 ...
 
 
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int res = 0;
+        int p[3] = {-1, -1, -1};
+
+        for (int i = 0; i < s.length(); i++) {
+            p[(s[i] & 31) - 1] = i;
+            res += min({p[0], p[1], p[2]}) + 1;
+        }
+
+        return res;
+    }
+};
+
+
+
+
 const numberOfSubstrings = s => {
     let res = 0, p = [5e4, -1, -1, -1];
 
