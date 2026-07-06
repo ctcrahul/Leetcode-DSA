230e6b34498ddc9a90cc2c9caea2332b1eb1232a
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-06 22:03:49 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -13,6 +13,20 @@ LLMs
 
 ...
 
+
+const removeCoveredIntervals = A => {
+    A.sort(([u, v], [x, y]) => (u - x) || (y - v));
+    let res = 0, lo = 0;
+
+    for (const [_, b] of A) {
+        res += (b > lo);
+        lo = Math.max(lo, b);
+    }
+
+    return res;
+};
+
+
 class Solution:
     def removeCoveredIntervals(self, A: List[List[int]]) -> int:
         A.sort(key=lambda x: (x[0], -x[1]))
