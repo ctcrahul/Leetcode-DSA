e01e52f1dc90334af29e0108a5b9e81b49d4a8d7
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-07 20:24:13 +0530

    Convert sumAndMultiply to Java syntax
    Refactor sumAndMultiply method for Java compatibility.

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -17,6 +17,20 @@ LLMs
 
 
 class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long x = 0, s = 0;
+        for (char c : to_string(n))
+            if (c != '0')
+                x = x * 10 + c - '0', s += c - '0';
+        return x * s;
+    }
+};
+
+
+
+
+class Solution {
     public long sumAndMultiply(int n) {
         long x = 0, s = 0;
         for (char c : String.valueOf(n).toCharArray())
