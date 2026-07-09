ae43c46ba3a1793c2d140a5d004d0a3962b1f19f
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-09 20:37:12 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,19 @@ LLMs
 ...
 
 
+class Solution {
+    public int countKdivPairs(int[] arr, int k) {
+        int[] ans = new int[k];
+        int count = 0;
+        for(int i:arr){
+            count+=ans[i%k];
+            ans[(-i%k+k)%k]++;
+        }
+        return count;
+    }
+}
+
+
 MOD, MAX = 1000000007, 100001
 pow = [1] * MAX
 for i in range(1, MAX):
