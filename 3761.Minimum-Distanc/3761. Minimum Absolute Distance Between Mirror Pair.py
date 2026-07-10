a77af4f56ad412a0202e69d6550b2e81624f9f91
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-10 20:08:37 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -13,7 +13,38 @@ LLMs
 
 ...
 
-
+class Solution {
+  public:
+    int getCount(int n) {
+      
+      if(n<=2)return 0;
+      int ans = 1;
+      int start = 1;
+      int end = 1;
+      int count = 0;
+      
+      int a = n/2;
+      a++;
+      
+      while(end<=a && start<=end){
+          
+          if(ans==n) count++;
+          
+          if(ans<=n){
+              end++;
+              ans+=end;
+          }
+          else{
+              ans-=start;
+              start++;
+          }
+          
+      }
+      
+      return count;
+      
+    }
+};
 
 
 class Solution {
