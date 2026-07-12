4d644258653af6d2267c9f4b36d1f7af91701635
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-12 20:41:47 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,23 @@ LLMs
 ...
 
 
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        sorted_arr = sorted(arr)
+
+        rank = {}
+        current_rank = 1
+
+        for num in sorted_arr:
+            if num not in rank:
+                rank[num] = current_rank
+                current_rank += 1
+
+        return [rank[num] for num in arr]
+
+
+
+
 class Solution {
   public:
     int maxAmount(vector<int>& arr, int k) {
