915d9dd738f5b868d9e5ea80a5fd677bb7d5b150
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-12 20:42:23 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,33 @@ LLMs
 ...
 
 
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+    const sorted = [...arr];
+
+    sorted.sort((a, b) => a - b);
+
+    const rank = new Map();
+    let currentRank = 1;
+
+    for (const num of sorted) {
+        if (!rank.has(num)) {
+            rank.set(num, currentRank++);
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = rank.get(arr[i]);
+    }
+
+    return arr;
+};
+
+
+
 func arrayRankTransform(arr []int) []int {
 	sorted := append([]int(nil), arr...)
 
