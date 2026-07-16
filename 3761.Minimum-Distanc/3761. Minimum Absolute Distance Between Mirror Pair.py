23f692bf353cb9ddfa4cdad2124546f28447b3d9
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-16 20:59:34 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -13,7 +13,25 @@ LLMs
 
 ...
 
+const gcd = (a, b) => !b ? a : gcd(b, a % b);
 
+const gcdSum = A => {
+    let max = 0;
+    const n = A.length;
+
+    for (let i = 0; i < n; i++) {
+        max = Math.max(max, A[i]);
+        A[i] = gcd(A[i], max);
+    }
+
+    A.sort((a, b) => a - b);
+
+    let res = 0;
+    for (let i = 0; i < n >> 1; i++)
+        res += gcd(A[i], A.at(-1 - i));
+
+    return res;
+};
 
 class Solution:
     def gcdSum(self, A: list[int]) -> int:
