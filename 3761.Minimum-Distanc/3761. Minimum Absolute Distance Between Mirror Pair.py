9d56849c4ad680d64f8b36cfa97e509095d6a3e5
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-19 21:24:38 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,31 @@ LLMs
 
 ...........
 
+
+	class Solution:
+    def processQueries(self, arr, queries):
+        # code here
+        pre = [1] * len(arr)
+        suff = [1] * len(arr)
+        
+        for i in range(1, len(arr)):
+            if arr[i-1] >= arr[i]:
+                pre[i] += pre[i-1]
+                
+                
+        for i in range(len(arr)-1, 0, -1):
+            if arr[i-1] <= arr[i]:
+                suff[i-1] += suff[i]
+                
+        res = []
+        for l, r in queries:
+            if suff[l] + pre[r] - 1 >= r - l + 1:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
+		
+
 class Solution {
     public int findGCD(int[] nums) {
         int mn = 1001, mx = 0;
