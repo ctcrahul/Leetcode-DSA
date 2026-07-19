0aa67f1327c3d280572bdc59b2a86cbec51cde4a
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-19 21:25:12 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -15,6 +15,27 @@ LLMs
 ...........
 
 
+const smallestSubsequence = s => {
+    const fq = _.countBy(s);
+    const vis = new Set();
+    const stack = [];
+
+    for (const c of s) {
+        fq[c]--;
+
+        if (vis.has(c)) continue;
+
+        while (stack.at(-1) > c && fq[stack.at(-1)])
+            vis.delete(stack.pop());
+
+        stack.push(c);
+        vis.add(c);
+    }
+
+    return stack.join``;
+};
+
+
 
 	class Solution:
     def smallestSubsequence(self, s: str) -> str:
