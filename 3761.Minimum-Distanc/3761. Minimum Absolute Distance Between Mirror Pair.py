f4f6c917ea5cf29f7d0fbb0432ccba6e48fbf405
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-23 21:31:56 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,44 @@ LLMs
 
 ...........
 
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def canRepresentBST(self, arr):
+
+        # Stack stores ancestor nodes
+        stack = []
+
+        # Smallest allowed value for future nodes
+        lower_bound = float("-inf")
+
+        # Traverse every preorder value
+        for value in arr:
+
+            # BST property is violated
+            if value < lower_bound:
+                return False
+
+            # Moving to the right subtree
+            while stack and value > stack[-1]:
+
+                # Update the lower limit
+                lower_bound = stack.pop()
+
+            # Store current node
+            stack.append(value)
+
+        # Entire preorder is valid
+        return True
+	
+
 class Solution:
     def preOrder(self, root):
         ans = []
