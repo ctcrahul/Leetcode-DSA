4a7cd84561f7df7096a6b94ea323f45fc1566c5a
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-23 21:32:26 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,12 @@ LLMs
 
 ...........
 
+	class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)        
+        return 1 << (n.bit_length() - 3 // (n + 1))
+
+
 '''
 # Node Class:
 class Node:
