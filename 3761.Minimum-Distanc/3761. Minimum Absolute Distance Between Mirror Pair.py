4093f3b3209d7b106942512ff15acfad7ee711d9
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-23 21:31:39 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,7 +14,20 @@ LLMs
 
 ...........
 
+class Solution:
+    def preOrder(self, root):
+        ans = []
 
+        def dfs(node):
+            if not node:
+                return
+            ans.append(node.data)
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ans
+	
 
 	/**
  * @param {string} s
