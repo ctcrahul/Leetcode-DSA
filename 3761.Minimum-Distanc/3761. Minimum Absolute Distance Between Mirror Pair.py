07eda6b722aefb2a0d2658aa0bf50693f33e997c
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-26 21:39:04 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -15,6 +15,16 @@ LLMs
 ...........
 
 
+class Solution:
+    def maximumProduct(self, A: List[int]) -> int:
+        A.sort()        
+        return max(
+            A[-1] * A[-2] * A[-3],
+            A[-1] * A[0] * A[1]
+        )
+
+
+
 
 class Solution(object):
     def maximumProduct(self, nums):
