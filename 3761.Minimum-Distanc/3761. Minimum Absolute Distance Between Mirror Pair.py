cc93ee6fb49799d76e03c03213f79e650b79ba1b
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-27 21:21:58 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -16,7 +16,36 @@ LLMs
 
 
 
-
+class Solution {
+    int i, j;
+    Node* createRecursively(vector<int> &pre, vector<int> &preM) {
+        Node* node = new Node(pre[i]);
+        
+        if(pre[i] == preM[j]) {
+            j--;
+            return node;
+        }
+        
+        int rootVal = pre[i];
+        
+        i++;
+        node->left = createRecursively(pre, preM);
+        
+        i++;
+        node->right = createRecursively(pre, preM);
+        
+        if(rootVal == preM[j]) j--;
+        
+        return node;
+    }
+  public:
+    Node *constructBinaryTree(vector<int> &pre, vector<int> &preMirror) {
+        i = 0;
+        j = preMirror.size() - 1;
+        
+        return createRecursively(pre, preMirror);
+    }
+};
 
 	const maximumProduct = A => {
     A.sort((a, b) => a - b);
