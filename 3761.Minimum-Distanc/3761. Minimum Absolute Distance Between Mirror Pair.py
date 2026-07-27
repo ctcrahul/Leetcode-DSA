866cb221588cbcf39eae931a7371a8ed7bad620a
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-27 21:22:35 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -16,6 +16,29 @@ LLMs
 
 
 
+
+	
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        first = 0
+
+        second = 0
+
+        for num in nums:
+
+            if num >= first:
+                second = first
+
+                first = num
+
+            elif num > second:
+                second = num
+
+        return (first - 1) * (second - 1)
+
+
+	
 class Solution {
     int i, j;
     Node* createRecursively(vector<int> &pre, vector<int> &preM) {
