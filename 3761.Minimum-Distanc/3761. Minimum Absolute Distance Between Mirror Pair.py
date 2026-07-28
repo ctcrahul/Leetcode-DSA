a5ba581629682713dcd29a18817bb48ff38f37c3
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-28 21:16:54 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -16,6 +16,23 @@ LLMs
 
 
 
+
+
+const smallestPalindrome = s => {
+    const n = s.length;
+    const f = _.countBy(s.slice(0, n >> 1));
+
+    const half = Object.keys(f).sort().map(c => c.repeat(f[c])).join``;
+    let mid = n & 1 ? s[n >> 1] : ``;
+    const rev = [...half].reverse().join``;
+
+    return half + mid + rev;
+};
+
+
+
+
+
 	class Solution {
 public:
     string smallestPalindrome(string s) {
