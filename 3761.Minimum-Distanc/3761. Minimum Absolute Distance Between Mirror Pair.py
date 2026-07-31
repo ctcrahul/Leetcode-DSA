d3bcefd716715e7a2e4b96eb66240aac6f8d12f5
Rahul Singh <rahulsingh51775177@gmail.com>
2026-07-31 20:17:52 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,26 @@ LLMs
 
 ...........
 
+
+
+	class Solution:
+    def minimumPushes(self, word: str) -> int:
+        char_count = [0] * 26
+
+        for ch in word:
+            char_count[ord(ch) - ord('a')] += 1
+
+        char_count.sort(reverse=True)
+
+        min_push_count = 0
+
+        for i in range(26):
+            min_push_count += char_count[i] * (i // 8 + 1)
+
+        return min_push_count
+
+
+
 class Solution {
 public:
     int minimumPushes(string word) {
