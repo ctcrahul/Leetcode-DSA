8a27fe67a29fd6815ca724a607de2c4d892bda08
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-01 21:56:56 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -16,6 +16,23 @@ LLMs
 
 
 
+
+
+class Solution:
+    def predictTheWinner(self, A: List[int]) -> bool:
+        n = len(A)
+        if ~n & 1: return True
+
+        dp = [0] * n
+
+        for i in range(n - 1, -1, -1):
+            dp[i] = A[i]
+            for j in range(i + 1, n):
+                dp[j] = max(A[i] - dp[j], A[j] - dp[j - 1])
+
+        return dp[n - 1] >= 0
+
+		
 	class Solution:
     def minimumPushes(self, word: str) -> int:
         char_count = [0] * 26
