7c2007dce49807f6a8d0710807fbb33ef60eac53
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-04 20:49:45 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,40 @@ LLMs
 
 ...........
 
+
+
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+
+        int mn = INT_MAX;
+        int mx = INT_MIN;
+
+        unordered_set<int> seen;
+
+        for (int x : nums) {
+
+            mn = min(mn, x);
+            mx = max(mx, x);
+
+            seen.insert(x);
+        }
+
+        vector<int> ans;
+
+        // Check every value inside the original range.
+        for (int x = mn + 1; x < mx; x++) {
+
+            if (!seen.count(x))
+                ans.push_back(x);
+        }
+
+        return ans;
+    }
+};
+
+
+
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
 
