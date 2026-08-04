9f8e67612f79309ef42c066ab9d64a8777b69cec
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-04 20:49:27 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,23 @@ LLMs
 
 ...........
 
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+
+        mn = min(nums)
+        mx = max(nums)
+
+        seen = set(nums)
+
+        ans = []
+
+        for x in range(mn + 1, mx):
+
+            if x not in seen:
+                ans.append(x)
+
+        return ans
+
 
 
 const stoneGame = A => 1;
