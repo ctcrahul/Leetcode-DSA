28f007493d83eba9c90697b142e32624e372f92b
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-05 21:18:07 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -16,6 +16,44 @@ LLMs
 
 
 
+class Solution {
+  public:
+  
+    int f(vector<int>&arr,int target)
+    {
+        int n = (int)arr.size();
+        int i =0;
+        int j = 0;
+        int s = 0;
+        int res = 0;
+        while(j<n)
+        {
+            s+=arr[j];
+            
+            while(i<=j && s>target)
+            {
+                s-=arr[i];
+                i++;
+            }
+            
+            res += (j-i+1);
+            j++;
+        }
+        return res;
+    }
+    int countSubarray(vector<int>& arr, int l, int r) {
+        // code here
+        int maxr = f(arr,r);
+        
+        int maxl = f(arr,l-1);
+        
+        // cout<<maxr<<" "<<maxl<<endl;
+        return (maxr-maxl);
+        
+    }
+};	
+
+
 
 
 var findMissingElements = function(nums) {
