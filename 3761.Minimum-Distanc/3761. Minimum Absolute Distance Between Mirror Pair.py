8a1eebef69c3a9837db0f2dd06759094f98f259c
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-06 19:27:00 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -15,6 +15,24 @@ LLMs
 ...........
 
 
+
+
+
+
+
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        q, r = divmod(n, 10)
+        
+        req = t // gcd(max(q, 1), t)
+        nxt = ((r + req - 1) // req) * req
+        x = nxt - (nxt - 10) * (nxt // 10)
+
+        return q * 10 + x
+
+
+
+
 class Solution {
   public:
     int countMinOperations(vector<int>& arr) {
