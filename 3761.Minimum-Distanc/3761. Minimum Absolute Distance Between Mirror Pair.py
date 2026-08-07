ed584b1e7d45dfec09cc746dde5e5d44e6028d0b
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-07 21:11:00 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -18,6 +18,131 @@ LLMs
 
 
 
+
+function smallestNumber(num: string, t: number): string {
+    const gcd = (a: number, b: number): number => {
+        while (b !== 0) {
+            const temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    };
+
+    let temp = t;
+
+    for (let digit = 2; digit <= 9; digit++) {
+        while (temp % digit === 0) {
+            temp /= digit;
+        }
+    }
+
+    if (temp !== 1) {
+        return "-1";
+    }
+
+    const n = num.length;
+    const digits: string[] = num.split("");
+
+    const remaining: number[] =
+        new Array(n + 1).fill(0);
+
+    remaining[0] = t;
+
+    let lastValidPos = n - 1;
+
+    for (let i = 0; i < n; i++) {
+        const digit = Number(digits[i]);
+
+        if (digit === 0) {
+            lastValidPos = i;
+            break;
+        }
+
+        remaining[i + 1] =
+            remaining[i] / gcd(remaining[i], digit);
+    }
+
+    if (remaining[n] === 1) {
+        return num;
+    }
+
+    for (let i = lastValidPos; i >= 0; i--) {
+        const currentDigit = Number(digits[i]);
+
+        for (
+            let newDigit = currentDigit + 1;
+            newDigit <= 9;
+            newDigit++
+        ) {
+            digits[i] = String(newDigit);
+
+            let need =
+                remaining[i] /
+                gcd(remaining[i], newDigit);
+
+            const suffix: string[] = [];
+
+            for (let j = i + 1; j < n; j++) {
+                let chosenDigit = 9;
+
+                while (
+                    chosenDigit > 1 &&
+                    need % chosenDigit !== 0
+                ) {
+                    chosenDigit--;
+                }
+
+                if (need % chosenDigit === 0) {
+                    need /= chosenDigit;
+                }
+
+                suffix.push(String(chosenDigit));
+            }
+
+            if (need === 1) {
+                suffix.reverse();
+
+                for (let j = i + 1; j < n; j++) {
+                    digits[j] =
+                        suffix[j - i - 1];
+                }
+
+                return digits.join("");
+            }
+        }
+
+        digits[i] = num[i];
+    }
+
+    const factors: string[] = [];
+    let remainingT = t;
+
+    for (let digit = 9; digit >= 2; digit--) {
+        while (remainingT % digit === 0) {
+            factors.push(String(digit));
+            remainingT /= digit;
+        }
+    }
+
+    const requiredLength = Math.max(
+        n + 1,
+        factors.length
+    );
+
+    while (factors.length < requiredLength) {
+        factors.push("1");
+    }
+
+    factors.reverse();
+
+    return factors.join("");
+}
+
+
+
+
+
 /**
  * @param {string} num
  * @param {number} t
