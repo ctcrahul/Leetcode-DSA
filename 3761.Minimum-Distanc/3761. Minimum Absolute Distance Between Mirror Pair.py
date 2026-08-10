81504e8ed68cb74d70437bab332a04315a0a1bc5
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-10 20:24:48 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,7 +14,23 @@ LLMs
 
 ...........
 
+class Solution {
+public:
+    static constexpr int MAX = 100000;
+    inline static bitset<MAX + 1> dp;
 
+    inline static bool init = []() {
+        for (int i = 0; i <= MAX; i++) {
+            if (dp.test(i)) continue;
+            for (int j = 1; j * j <= MAX - i; j++)
+                dp.set(i + j * j);
+        }
+        
+        return false;
+    }();
+
+    bool winnerSquareGame(int n) { return dp.test(n); }
+};
 
 class Solution:
     MAX = 10**5
