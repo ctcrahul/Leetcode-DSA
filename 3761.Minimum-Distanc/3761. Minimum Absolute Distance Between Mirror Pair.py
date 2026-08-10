a25770b36406aaf0c8619cad9906dccf40a7d902
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-10 20:25:09 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,34 @@ LLMs
 
 ...........
 
+
+
+
+class Solution {
+  public:
+    int maxTask(vector<int>& h, vector<int>& l) {
+        int n = h.size();
+
+        int prev1 = max(h[0], l[0]);
+
+        int prev2 = 0;
+
+        for (int i = 1; i < n; i++) {
+            int lowChoice = prev1 + l[i];
+
+            int highChoice = prev2 + h[i];
+
+            int current = max(lowChoice, highChoice);
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+};
+
+
 class Solution {
 public:
     static constexpr int MAX = 100000;
