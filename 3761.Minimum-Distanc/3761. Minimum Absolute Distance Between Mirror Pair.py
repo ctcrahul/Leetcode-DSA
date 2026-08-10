b9af32cb79f368c62c190e6a1448d6cadb6af285
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-10 20:24:24 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -15,6 +15,21 @@ LLMs
 ...........
 
 
+
+class Solution:
+    MAX = 10**5
+    dp = [False] * (MAX + 1)
+
+    for i in range(MAX + 1):
+        if dp[i]: continue
+
+        for j in range(1, isqrt(MAX - i) + 1):
+            dp[i + j**2] = True
+
+    def winnerSquareGame(self, n: int) -> bool:
+        return self.dp[n]
+
+
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         for i in range(len(piles) - 2, -1, -1):
