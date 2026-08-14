618a05fcaa3e61a52e07906b64d3065e0f64ef79
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-14 20:50:06 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,27 @@ LLMs
 
 ...........
 
+
+
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int res = 0;
+        int fq[26] = {0};
+
+        for (int l = 0, r = 0; r < s.length(); r++) {
+            fq[(s[r] & 31) - 1]++;
+
+            while (fq[(s[r] & 31) - 1] > 2)
+                fq[(s[l++] & 31) - 1]--;
+
+            res = max(res, r - l + 1);
+        }
+
+        return res;
+    }
+};
+
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
         res = l = 0
