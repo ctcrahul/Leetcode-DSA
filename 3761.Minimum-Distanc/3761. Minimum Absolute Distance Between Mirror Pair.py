9282dc3fa0f154360cddf5c00648f67c9dc62c27
Rahul Singh <rahulsingh51775177@gmail.com>
2026-08-18 20:34:10 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pair.py

--- 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
+++ 3761.Minimum-Distanc/3761. Minimum Absolute Distance Between Mirror Pair.py
@@ -14,6 +14,28 @@ LLMs
 
 ...........
 
+
+
+
+class Solution {
+public:
+    int largestInteger(vector<int>& A, int k) {
+        int f[51] = {0};
+        for (auto& x : A)
+            f[x]++;
+
+        int res = -1, n = A.size();
+        for (int i = 0; i < n; i++)
+            if (k == n || (f[A[i]]==1 && (k==1||!i||i==n-1)))
+                res = max(res, A[i]);
+
+        return res;
+    }
+};
+
+
+
+
 class Solution:
     def largestInteger(self, A: List[int], k: int) -> int:
         f = [0] * 51
