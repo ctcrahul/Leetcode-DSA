9ef98689961ab32815c755043958194b1935fa1a
Rahul Singh <rahulsingh51775177@gmail.com>
2026-09-10 20:47:13 +0530

    Implement countCommas method in Solution class
    Add a method to count commas in a number based on thousands.

--- 3755. Find Maximum Balanced XOR Subarray Length/3755. Find Maximum Balanced XOR Subarray Length.py
+++ 3755. Find Maximum Balanced XOR Subarray Length/3755. Find Maximum Balanced XOR Subarray Length.py
@@ -25,3 +25,16 @@ class Solution:
 
 today machine learning practicAL paper
 ///
+
+
+
+
+class Solution:
+    def countCommas(self, n: int) -> int:
+        count, p = 0, 1000
+
+        while p <= n:
+            count += n - p + 1
+            p *= 1000
+
+        return count
